44402e4e71b6637db3fcf1c1442d0e558f92b392
inoue <inoue@www3441ue.sakura.ne.jp>
2014-09-18 21:52:07 +0900

    initial app

---
+++ strava-feed.rb
@@ -0,0 +1,62 @@
+#!/bin/env ruby
+
+require 'logger'
+require 'date'
+require 'strava/api/v3'
+
+class StravaFeed
+  def initialize(logger = nil)
+    if logger then
+      @logger = logger
+    else
+      @logger = Logger.new(STDOUT) 
+      @logger.level = Logger::INFO
+    end
+    @fileout = STDOUT
+    @client = Strava::Api::V3::Client.new(:access_token => "f641bc0eac932eff658633e8dfc37d4b88efacb5", :logger => @logger)
+  end
+  
+  def get_activities
+    @activities = @client.list_athlete_activities
+    # http://strava.github.io/api/v3/activities/
+  end
+
+  def puts_activities
+    get_activities
+    puts "#start_time, km" # csv format header
+    @activities.map{|act| 
+      date = DateTime.parse(act["start_date"]).new_offset(Rational(9,24))
+      
+      @fileout.print "#{date}, " 
+      @fileout.puts  "#{act["distance"]/1000.0}" 
+    }
+  end
+  #puts_activities
+
+  def puts_daily_stats
+    get_activities
+    offset = Rational(9,24)
+    stats = Hash.new(0.0)
+
+    puts "#date, km" # csv format header
+    @activities.map{|act| 
+      datetime = DateTime.iso8601(act["start_date"])
+      date = (datetime + offset).to_date
+      stats[date] += act["distance"]/1000.0
+    }
+    #p stats
+    #stats.each{|date, km| @fileout.puts "#{date}, #{km}"  }
+
+    min, max = stats.keys.minmax
+    (min..max).each{|date|
+      @fileout.puts "#{date}, #{stats[date]}" 
+    }
+  end
+  #puts_daily_stats
+end
+
+# Run only when TopLevel (not perfect but works...)
+if (self.to_s == "main") then
+  StravaFeed.new.puts_daily_stats
+end
+  
